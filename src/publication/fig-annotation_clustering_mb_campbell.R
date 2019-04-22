48df62b25042720d88e4736f7ef6b0b8a7fa18c7
Pascal Timshel at Yggdrasil <pascal.timshel@gmail.com>
2019-04-22 13:15:35 +0200

    updated hypothalamus integrated fig to include aligned dendrogram.

--- src/publication/fig-annotation_clustering_mb_campbell.R
+++ src/publication/fig-annotation_clustering_mb_campbell.R
@@ -24,96 +24,105 @@ library(corrr)
 source(here("src/lib/load_functions.R")) # load sc-genetics library
 source(here("src/publication/lib-load_pub_lib_functions.R"))
 
-setwd(here("src/publication"))
-
 
 # ======================================================================= #
 # ============================ PARAMETERS =============================== #
 # ======================================================================= #
 
 
+get_mousebrain_campbell_integrated_dendrogram <- function(save_fig) {
+  
+  # ======================================================================= #
+  # ================================== MOUSEBRAIN ========================= #
+  # ======================================================================= #
+  dataset_prefix <- "mousebrain_all"
+  df.metadata.mb <- get_metadata(dataset_prefix)
+  ### Annotation ESmu
+  file.es <- here(sprintf("out/es/%s.mean.csv.gz", dataset_prefix)) # New ESmu values
+  df.es <- read_csv(file.es) # genes x cell-types
+  
+  ### Filter
+  filter.annotations <- get_annotations.mousebrain.hypothalamus()
+  df.es.mb <- df.es %>% select(gene, filter.annotations)
+  
+  # ======================================================================= #
+  # ================================== CAMPBELL ========================= #
+  # ======================================================================= #
+  dataset_prefix <- "campbell_lvl2"
+  df.metadata.cb <- get_metadata(dataset_prefix)
+  
+  ### Annotation ESmu
+  file.es <- here(sprintf("out/es/%s.mean.csv.gz", dataset_prefix)) # New ESmu values
+  df.es.cb <- read_csv(file.es) # genes x cell-types
+  
+  ### Filter | *CONSIDER ONLY INCLUDING CAMPBELL NEURONS*
+  # df.es.cb %>% select(gene, filter.annotations)
+  
+  # ======================================================================= #
+  # ================================== MERGE DATA FRAMES ========================= #
+  # ======================================================================= #
+  
+  # dim(df.es.cb) # 12509    65
+  # dim(df.es.mb) # 15071    18
+  df.es <- inner_join(df.es.cb, df.es.mb, by="gene")
+  
+  # ======================================================================= #
+  # ================================== MAKE META-DATA ========================= #
+  # ======================================================================= #
+  
+  df.metadata.join <- tibble(
+    annotation=colnames(df.es)[-1] # exclude gene
+  )
+  
+  df.metadata.join <- df.metadata.join %>% mutate(dataset=if_else(annotation %in% df.metadata.mb$annotation, "Mousebrain", "Campbell"))
+  df.metadata.join
+  
+  # ======================================================================= #
+  # ======================== CALCULATE CORR/DENDROGRAM ===================== #
+  # ======================================================================= #
+  
+  # ============================ Calculate correlation =============================== #
+  cormat.es <- cor(df.es %>% select(-gene), method="pearson") 
+  
+  # ========================= [base] Calculate dendrogram ============================= #
+  
+  ### Compute distances and hierarchical clustering
+  # dd.eucledian <- dist(t(df.es %>% select(-gene)), method = "euclidean") # computes distances between the rows of a data matrix
+  dd.corr <- as.dist(1-cormat.es)
+  hc <- hclust(dd.corr, method = "average") # Hierarchical clustering 
+  # single linkage = nearest neighbour. 
+  # complete linkage = farthest neighbour
+  # ward distance does not have easy interpretable dedrogram 'height' for correlation distances.
+  dend <- as.dendrogram(hc) # Turn the object into a dendrogram.
+  # plot(dend)
+  
+  
+  # ======================================================================= #
+  # =========================== plot dendrogram =========================== #
+  # ======================================================================= #
+  
+  ### 'Linear'
+  p.linear <- plot_es_dendrogram.mb_campbell(dend, df.metadata.join, circular=FALSE)
+  if (save_fig) {
+    file.out <- sprintf("figs/fig_clustering.%s.dendrogram.linear.pdf", dataset_prefix="integrated_campbell_mousebrain")
+    ggsave(plot=p.linear, filename=file.out, width=9, height=4)
+  }
+  
+  # ### Circular
+  # p.circular <- plot_es_dendrogram.mb_campbell(dend, df.metadata.join, circular=TRUE)
+  # file.out <- sprintf("figs/fig_%s.dendrogram.circular.pdf", dataset_prefix="integrated_campbell_mousebrain")
+  # ggsave(plot=p.circular, filename=file.out, width=6, height=6)
+  
+  
+  list.res <- list(plot=p.linear, dendrogram=dend)
+  return(list.res)
+}
 
-
-# ======================================================================= #
-# ================================== MOUSEBRAIN ========================= #
-# ======================================================================= #
-dataset_prefix <- "mousebrain_all"
-df.metadata.mb <- get_metadata(dataset_prefix)
-### Annotation ESmu
-file.es <- here(sprintf("out/es/%s.mean.csv.gz", dataset_prefix)) # New ESmu values
-df.es <- read_csv(file.es) # genes x cell-types
-
-### Filter
-filter.annotations <- get_annotations.mousebrain.hypothalamus()
-df.es.mb <- df.es %>% select(gene, filter.annotations)
-
-# ======================================================================= #
-# ================================== CAMPBELL ========================= #
-# ======================================================================= #
-dataset_prefix <- "campbell_lvl2"
-df.metadata.cb <- get_metadata(dataset_prefix)
-
-### Annotation ESmu
-file.es <- here(sprintf("out/es/%s.mean.csv.gz", dataset_prefix)) # New ESmu values
-df.es.cb <- read_csv(file.es) # genes x cell-types
-
-### Filter | *CONSIDER ONLY INCLUDING CAMPBELL NEURONS*
-# df.es.cb %>% select(gene, filter.annotations)
-
-# ======================================================================= #
-# ================================== MERGE DATA FRAMES ========================= #
-# ======================================================================= #
-
-# dim(df.es.cb) # 12509    65
-# dim(df.es.mb) # 15071    18
-df.es <- inner_join(df.es.cb, df.es.mb, by="gene")
-
-# ======================================================================= #
-# ================================== MAKE META-DATA ========================= #
-# ======================================================================= #
-
-df.metadata.join <- tibble(
-  annotation=colnames(df.es)[-1] # exclude gene
-)
-
-df.metadata.join <- df.metadata.join %>% mutate(dataset=if_else(annotation %in% df.metadata.mb$annotation, "Mousebrain", "Campbell"))
-df.metadata.join
-
-# ======================================================================= #
-# ======================== CALCULATE CORR/DENDROGRAM ===================== #
-# ======================================================================= #
-
-# ============================ Calculate correlation =============================== #
-cormat.es <- cor(df.es %>% select(-gene), method="pearson") 
-
-# ========================= [base] Calculate dendrogram ============================= #
-
-### Compute distances and hierarchical clustering
-# dd.eucledian <- dist(t(df.es %>% select(-gene)), method = "euclidean") # computes distances between the rows of a data matrix
-dd.corr <- as.dist(1-cormat.es)
-hc <- hclust(dd.corr, method = "average") # Hierarchical clustering 
-# single linkage = nearest neighbour. 
-# complete linkage = farthest neighbour
-# ward distance does not have easy interpretable dedrogram 'height' for correlation distances.
-dend <- as.dendrogram(hc) # Turn the object into a dendrogram.
-plot(dend)
-
-
-# ======================================================================= #
-# =========================== plot dendrogram =========================== #
-# ======================================================================= #
-
-source(here("src/publication/lib-load_pub_lib_functions.R"))
-
-### 'Linear'
-p.linear <- plot_es_dendrogram.mb_campbell(dend, df.metadata.join, circular=FALSE)
-file.out <- sprintf("figs/fig_clustering.%s.dendrogram.linear.pdf", dataset_prefix="integrated_campbell_mousebrain")
-ggsave(plot=p.linear, filename=file.out, width=9, height=4)
-
-
-# ### Circular
-# p.circular <- plot_es_dendrogram.mb_campbell(dend, df.metadata.join, circular=TRUE)
-# file.out <- sprintf("figs/fig_%s.dendrogram.circular.pdf", dataset_prefix="integrated_campbell_mousebrain")
-# ggsave(plot=p.circular, filename=file.out, width=6, height=6)
+# interactive(): returns TRUE when R is being used interactively and FALSE otherwise
+# REF: https://stackoverflow.com/a/2968404/6639640
+if (!interactive()) { # function will run if script is sourced when running non-interactively (e.g. called via Rscript)
+  setwd(here("src/publication"))
+  get_mousebrain_campbell_integrated_dendrogram(save_fig=TRUE)
+}
 
 
--- src/publication/fig-celltype_priori_hypo_mb_campbell.R
+++ src/publication/fig-celltype_priori_hypo_mb_campbell.R
@@ -144,13 +144,80 @@ df.join <- df.join %>% mutate(p.value.mlog10 = -log10(p.value))
 ### Add enrichment data
 df.join <- df.join %>% left_join(df.enrich %>% select(annotation_clean, combined_rare_mendelian_obesity), by="annotation_clean")
 
+# ======================================================================= #
+# ============================ GET DENDROGRAM =========================== #
+# ======================================================================= #
+
+source("fig-annotation_clustering_mb_campbell.R")
+list.res <- get_mousebrain_campbell_integrated_dendrogram(save_fig=FALSE)
+p.dendro.orig <- list.res[["plot"]]
+dend <- list.res[["dendrogram"]]
+
+# ======================================================================= #
+# ============================ MODIFY DENDROGRAM =========================== #
+# ======================================================================= #
+
+
+# ============================ FUNCTION =========================== #
+remove_geom <- function(ggplot2_object, geom_type) {
+  # COPY FROM https://stackoverflow.com/a/46221054/6639640
+  # ALTERNATIVE: https://rdrr.io/cran/ggedit/man/remove_geom.html
+  # Delete layers that match the requested type.
+  layers <- lapply(ggplot2_object$layers, function(x) {
+    if (class(x$geom)[1] == geom_type) {
+      NULL
+    } else {
+      x
+    }
+  })
+  # Delete the unwanted layers.
+  layers <- layers[!sapply(layers, is.null)]
+  ggplot2_object$layers <- layers
+  ggplot2_object
+}
+
+### TEST FUNCTION
+# lapply(p.dendro.orig$layers, function(x) {print(class(x$geom)[1])})
+
+# ============================ MODIFY =========================== #
+n_leaf_nodes <- length(order.dendrogram(dend))
+
+p.dendro <- p.dendro.orig # init
+p.dendro <- p.dendro + theme_classic() # "reset" theme settings 
+p.dendro <- p.dendro + coord_flip(clip="off",
+                                  expand=F,
+                                  xlim=c(-1,n_leaf_nodes-0.5) # OBS: not sure why the "-0.5" is needed, but it helps the alingment with LDSC plot.
+                                  # ^ xlim is needed for perfect/correct alingment with ldsc plot
+                                  # ^ xlim "zooms in" without removing data | REF https://stackoverflow.com/questions/25685185/limit-ggplot2-axes-without-removing-data-outside-limits-zoom
+                                  # the initially plot is contructed with " p + xlim(c(-1,n_leaf_nodes+1))"
+                                  )
+# p.dendro <- p.dendro + xlim(c(0,n_leaf_nodes)) # this seems to help the alignment with LDSC if scale_y_continuous(position="bottom) is enabled
+p.dendro <- p.dendro + scale_y_continuous(trans="reverse" # same as scale_y_reverse()
+                                          # position="bottom" # put axis on top | works semi-ok
+                                          )
+p.dendro <- p.dendro + guides(color=F) # no guides for dataset color
+p.dendro <- p.dendro + theme(axis.line.y=element_blank(), # remove all x-axis 
+                             axis.title.y=element_blank(),
+                             axis.ticks.y=element_blank(),
+                             axis.text.y=element_blank())
+p.dendro <- remove_geom(p.dendro, "GeomText") # remove old node text
+p.dendro.w_labels <- p.dendro + geom_node_text(aes(filter=(leaf==TRUE), color=dataset, label=label), # add new node text [only needed temporarily to check that patchwork alings correctly]
+                        size=rel(1.2), angle=0, hjust="left", nudge_y=0.01, 
+                        show.legend=F)
+p.dendro.w_labels <- p.dendro.w_labels + theme(plot.margin = unit(c(1,3,1,1), "cm")) # (t, r, b, l) widen margin
+# p.dendro.w_labels
 
 # ======================================================================= #
 # ================================ PLOT LDSC ================================ #
 # ======================================================================= #
 
-### Colormap (*SHOULD BE THE SAME AS IN lib-annotation_clustering plot_es_dendrogram.mb_campbell()*)
+### Order annotations by dendrogram
+order.annotations <- labels(dend) # labels(dend) returns ordered labels | order.dendrogram(dend) returns ordered index.
+order.annotations <- str_split(order.annotations, pattern="\\.") %>% purrr::map_chr(1) # clean names
+df.join <- df.join %>% mutate(annotation_clean = factor(annotation_clean, levels=order.annotations))
 
+
+### Colormap (*SHOULD BE THE SAME AS IN lib-annotation_clustering plot_es_dendrogram.mb_campbell()*)
 dataset_names <- sort(unique(df.join$dataset))
 colormap.dataset <- brewer.pal(name="Set1", n=9) # n=9 max for Set1 | "Set2"/"Dark2" ok for colorblind
 colormap.dataset <- colormap.dataset[1:length(dataset_names)] # select the colors we need
@@ -172,25 +239,50 @@ p.priori <- ggplot() +
   # geom_text(data=df.join, aes(x=annotation_clean, y=p.value.mlog10, label=text), size=rel(2), angle=90, vjust=0.22, hjust=-0.08, show.legend=F) +
   # ^ this works when no using coord_flip()
   ### axes
-  labs(x="", y=expression(-log[10](P[S-LDSC])), size=expression(-log[10](P[enrichment]))) +
+  labs(x="", y=expression(-log[10](P[S-LDSC])), size=expression(-log[10](P[enrichment])), fill="") +
   # coord
   coord_flip(clip = 'off') + # This keeps the labels from disappearing 
-  ### guides
-  #...
   ### color
   scale_color_manual(values=colormap.dataset) +
   scale_fill_manual(values=colormap.dataset) +
+  ### guides
+  guides(stroke=F, color=F,
+         fill=guide_legend(order = 1),
+         size=guide_legend(order = 2)) + 
   ### theme
   theme_classic() + 
   theme(axis.text.y=element_text(size=rel(0.6)),
         axis.ticks.y=element_blank()) +
   # theme(axis.text.x=element_text(angle=65, hjust=1, size=rel(0.6))) + # no coord_flip
-  theme(legend.position="right")
+  theme(legend.position="top")
 ### Add margin to plot if displaying it directly (without pathwork)
+p.priori <- p.priori + theme(plot.margin = unit(c(1,3,1,1), "cm")) # (t, r, b, l) widen margin
 p.priori
 
 file.out <- "figs/fig_celltypepriori.integrated_mb_campbell.priori.pdf"
 ggsave(plot=p.priori, filename=file.out, width=8, height=8)
+
+
+
+# ======================================================================= #
+# ======================== PATCHWORK: PRIORI + DENDROGRAM ==================== #
+# ======================================================================= #
+
+### Check alignment ok
+# p.dendro.w_labels + p.priori
+
+### Make patchwork
+p.patch <- p.dendro + p.priori + plot_spacer() + plot_layout(nrow=1, widths=c(0.3,1, 0.001))
+p.patch
+
+file.out <- "figs/fig_celltypepriori.integrated_mb_campbell.priori_with_dendrogram.pdf"
+ggsave(plot=p.patch, filename=file.out, width=8, height=8)
+
+
+### [OK DELETE] Debug alingment of plots:
+# layer_scales(p.dendro)$x$range$range # get x-axis of barplot | REF: https://stackoverflow.com/a/35372274/6639640
+# layer_scales(p.priori)$x$range$range
+
 
 # =================================================================== #
 # =========================== PLOT ENRICHMENT ======================= #
@@ -232,7 +324,7 @@ p.enrich.mod
 
 
 # ======================================================================= #
-# ================================ PATCHWORK ================================ #
+# ======================== PATCHWORK: PRIORI + ENRIH ==================== #
 # ======================================================================= #
 
 p.patch <- p.enrich.mod + p.priori
@@ -242,9 +334,26 @@ file.out <- "figs/fig_celltypepriori.integrated_mb_campbell.priori_with_enrichme
 ggsave(plot=p.patch, filename=file.out, width=8, height=8)
 
 
+
 # ======================================================================= #
 # ================================ XXXXX ================================ #
 # ======================================================================= #
+
+
+# ======================================================================= #
+# ================================ XXXXX ================================ #
+# ======================================================================= #
+
+
+# ======================================================================= #
+# ================================ XXXXX ================================ #
+# ======================================================================= #
+
+
+
+
+
+
 
 
 

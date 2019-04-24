6814dcedb63b873874d3faf5a1d4e8266d726a88
Pascal Timshel at Yggdrasil <pascal.timshel@gmail.com>
2019-04-24 13:18:07 +0200

    update new genes

--- src/publication/fig-es_genes.R
+++ src/publication/fig-es_genes.R
@@ -24,6 +24,58 @@ library(patchwork)
 
 setwd(here("src/publication"))
 
+
+# ======================================================================= #
+# ============================== GENE WIKI ============================= #
+# ======================================================================= #
+
+### CCK
+# "One mechanism proposed to explain how leptin reduces food intake is by enhancing the CNS response to gut-derived satiety peptides, 
+# such as cholecystokinin (CCK) and glucagon-like peptide-1 (GLP-1) (196) that are released upon food ingestion and activate 
+# vagal afferent fibers that terminate in the NTS" (http://physrev.physiology.org/content/91/2/389.short p. 401ff)
+
+# Receptors: Cckar (Cck1), Cckbr (Cck2) 
+# M. Schwartz thinks this is an important cell-type.
+# Cckbr is specifically expressed in "n29.Nr5a1/Bdnf" cell-type 
+
+### GDF15 seems to bind to its receptor (GFRAL) in the area postrema
+### Hdac5 (which encodes histone deacetylase 5) is expressed in specific hypothalamic neurons that are activated by leptin.
+### FAM46A is a trans regulator for leptin
+
+### PDYN / MC4R
+# PVH-PDYN and PVH-MC4R neurons use separate efferent circuits to regulate appetite
+# Li et al., 2019, Neuron (https://doi.org/10.1016/j.neuron.2019.02.028)
+# "This expands the CNS satiety circuitry to include two non-overlapping PVH to hindbrain circuits."
+
+### Ciliopathy-related proteins [ADYC3 like] 
+# genes_select <- c("MRAP2","ALMS1","MKS1","CEP290","FRITZ","C2ORF86","SDCCAG8","CEP19","ANKRD26")
+# genes_select <- c("BBS1","BBS2","BBS3","BBS4","BBS5","BBS6","BBS7","BBS8","BBS9","BBS10","BBS11","BBS12")
+
+
+### Somatostatin
+# Humans have only one somatostatin gene, SST. 
+# Receptor: Five receptors.
+
+### ghrelin, 
+# Ghrl=ghrelin gene
+# Receptor: "Ghsr"=ghrelin/growth hormone secretagogue receptor
+
+### endorphins
+# The class of endorphins includes three compounds—α-endorphin (alpha endorphins), β-endorphin (beta endorphins), and γ-endorphin (gamma endorphins)
+# --> POMC
+
+### vasopressin=AVP 
+# Vasopressin, also called antidiuretic hormone (ADH), arginine vasopressin (AVP) or argipressin 
+# Receptors [REF=Wiki] = "AVPR1A","AVPR1B","AVPR3","AVPR2"
+
+### serotonin
+# RECEPTORS [REF=Wiki]: c("HTR1A", "HTR1B", "HTR1D", "HTR1E", "HTR1F", "HTR2A", "HTR2B", "HTR2C", "HTR3A", "HTR3B", "HTR3C", "HTR3D", "HTR3E", "HTR4", "HTR5A", "HTR5BP", "HTR6", "HTR7")
+# HTR1A: serotonin 1A receptor
+
+### GIP: Gastric inhibitory polypeptide
+# Incretin like GLP-1
+# Receptor: GIPR
+
 # ======================================================================= #
 # ============================== LOAD DATA ============================== #
 # ======================================================================= #
@@ -67,6 +119,7 @@ file.out <- "figs/fig_es.mb.pomc_agrp_lepr_mc4r.avg_expr.pdf"
 ggsave(plot=p, filename=file.out, width=8.5, height=5)
 p
 
+
 # ======================================================================= #
 # =================== SOM FIG: INDIVUDAL GENES *COMBINED* ================ #
 # ======================================================================= #
@@ -102,12 +155,15 @@ ggsave(filename="figs/fig_es.mb.drdx_genes.pdf", plot=p, width=12, height=5) # A
 
 # ======================= Standard model genes [BMI + appetite regulation] ======================= #
 
-genes_select <- c("Npy", "Agrp", "Pomc", "Cartpt", "Lepr", "Insr", "Trh", "Cck", "Glp1r") 
+
+genes_select <- c("Agrp", "Pomc", "Cartpt", "Lepr", "Insr", "Glp1r") 
+genes_select <- c(genes_select, "Npy", "PYY", "NPY1R","NPY2R","PPYR1","NPY5R") # NPY and PYY. PYY binds to NPY receptors
+genes_select <- c(genes_select, "Ghrl", "Ghsr")
+genes_select <- c(genes_select, "Cck", "Cckar", "Cckbr")
 genes_select <- c(genes_select, "Ntrk2")
-genes_select <- c(genes_select, "Bdnf", "Cadm2", "Negr1") # known GWAS genes
-genes_select <- c(genes_select, "Gdf15", "Gfral", "Hdac5", "Fam46a") # Hindbrain
-genes_select <- c(genes_select, "Cckar", "Cckbr") # Cckar (Cck1), Cckbr (Cck2) # Schwartz thinks this is an important cell-type. ---> Cckbr is specifically expressed in "n29.Nr5a1/Bdnf" cell-type 
-genes_select <- c(genes_select, "Sim1", "Pdyn") # Li et al., 2019, Neuron (https://doi.org/10.1016/j.neuron.2019.02.028)
+genes_select <- c(genes_select, "Bdnf", "Cadm2", "Negr1")
+genes_select <- c(genes_select, "Gdf15", "Gfral", "Hdac5", "Fam46a") 
+genes_select <- c(genes_select, "Sim1", "Pdyn") 
 genes_select <- toupper(genes_select)
 df.es.gene <- get_es.gene_centric.single_es_metric(sem_obj, genes_select=genes_select, es_metric="es_mu")
 p <- plot_es.gene_centric.single_es_metric(df.es.gene, 
@@ -116,17 +172,44 @@ p <- plot_es.gene_centric.single_es_metric(df.es.gene,
 p <- p + labs(y=expression(ES[mu]))
 ggsave(filename="figs/fig_es.mb.standard_model_genes_and_more.pdf", plot=p, width=10, height=12) # A4 8.3 x 11.7
 
+# ======================= Serotonin receptors ======================= #
+
+genes_select <- c("HTR1A", "HTR1B", "HTR1D", "HTR1E", "HTR1F", "HTR2A", "HTR2B", "HTR2C", "HTR3A", "HTR3B", "HTR3C", "HTR3D", "HTR3E", "HTR4", "HTR5A", "HTR5BP", "HTR6", "HTR7")
+df.es.gene <- get_es.gene_centric.single_es_metric(sem_obj, genes_select=genes_select, es_metric="es_mu")
+p <- plot_es.gene_centric.single_es_metric(df.es.gene, 
+                                           annotations_highlight=annotations_highlight,
+                                           annotations_colormap=colormap.annotation)
+p <- p + labs(y=expression(ES[mu]))
+ggsave(filename="figs/fig_es.mb.serotonin.pdf", plot=p, width=10, height=12) # A4 8.3 x 11.7
+
+
+# ======================= Early/stress response genes ======================= #
+
+genes_select <- c("Fos", "Jun", "Junb", "Egr1")
+genes_select <- toupper(genes_select)
+df.es.gene <- get_es.gene_centric.single_es_metric(sem_obj, genes_select=genes_select, es_metric="es_mu")
+p <- plot_es.gene_centric.single_es_metric(df.es.gene, 
+                                           annotations_highlight=annotations_highlight,
+                                           annotations_colormap=colormap.annotation)
+p <- p + labs(y=expression(ES[mu]))
+ggsave(filename="figs/fig_es.mb.early_response_genes.pdf", plot=p, width=8, height=5) # A4 8.3 x 11.7
 
 # ======================= ALL GENES [*KEEP ME UPDATED*] ======================= #
 
 genes_select <- c("MC1R", "MC2R", "MC3R", "MC4R", "MC5R", "ADCY3")
 genes_select <- c(genes_select, "FTO", "IRX3", "IRX5")
-genes_select <- c(genes_select, "DRD2", "DRD1", "DRD3")
-genes_select <- c(genes_select, "Npy", "Agrp", "Pomc", "Cartpt", "Lepr", "Insr", "Trh", "Cck", "Glp1r") 
+genes_select <- c(genes_select, "Agrp", "Pomc", "Cartpt", "Lepr", "Insr", "Glp1r") 
+genes_select <- c(genes_select, "Ghrl", "Ghsr") # Grelin
+genes_select <- c(genes_select, "Cck", "Cckar", "Cckbr")
+genes_select <- c(genes_select, "Npy", "PYY", "NPY1R","NPY2R","PPYR1","NPY5R") # NPY and PYY. PYY binds to NPY receptors
+genes_select <- c(genes_select, "GIP", "GIPR")
+genes_select <- c(genes_select, "Sst", "SSTR1","SSTR2","SSTR3","SSTR4","SSTR5") # Somatostatin
+genes_select <- c(genes_select, "Oxt", "Oxtr") # Oxytocin
+genes_select <- c(genes_select, "Avp", "AVPR1A","AVPR1B","AVPR3","AVPR2")  # Vasopressin
+genes_select <- c(genes_select, "DRD2", "DRD1", "DRD3") # Dopamine
 genes_select <- c(genes_select, "Ntrk2")
 genes_select <- c(genes_select, "Bdnf", "Cadm2", "Negr1") # known GWAS genes
 genes_select <- c(genes_select, "Gdf15", "Gfral", "Hdac5", "Fam46a") # Hindbrain
-genes_select <- c(genes_select, "Cckar", "Cckbr") # Cckar (Cck1), Cckbr (Cck2) # Schwartz thinks this is an important cell-type. ---> Cckbr is specifically expressed in "n29.Nr5a1/Bdnf" cell-type 
 genes_select <- c(genes_select, "Sim1", "Pdyn") # Li et al., 2019, Neuron (https://doi.org/10.1016/j.neuron.2019.02.028)
 genes_select <- toupper(genes_select)
 df.es.gene <- get_es.gene_centric.single_es_metric(sem_obj, genes_select=genes_select, es_metric="es_mu")
